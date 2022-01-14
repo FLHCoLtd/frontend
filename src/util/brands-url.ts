099d97c5e762984924fa7b2580744aa564b22c08
wayne <wayne.tu@flh.com.tw>
2022-01-14 13:42:19 +0800

    add icon url for flh component

--- src/util/brands-url.ts
+++ src/util/brands-url.ts
@@ -5,7 +5,16 @@ export interface BrandsOptions {
   darkOptimized?: boolean;
 }
 
-export const brandsUrl = (options: BrandsOptions): string =>
-  `https://brands.home-assistant.io/${options.useFallback ? "_/" : ""}${
-    options.domain
-  }/${options.darkOptimized ? "dark_" : ""}${options.type}.png`;
+export const brandsUrl = (options: BrandsOptions): string => {
+  if (options.domain === "ferqo_cc" || options.domain === "line_notify") {
+    return `http://127.0.0.1:8123/local/icon/${options.domain}/${
+      options.darkOptimized ? "dark_" : ""
+    }${options.type}.png`;
+  } 
+    return `https://brands.home-assistant.io/${
+      options.useFallback ? "_/" : ""
+    }${options.domain}/${options.darkOptimized ? "dark_" : ""}${
+      options.type
+    }.png`;
+  
+};
